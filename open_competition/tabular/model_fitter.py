0dd8abdfd72fe7d455eb587aa8872b06286a26b5
rwbfd <ran.wang.math@gmail.com>
2020-08-26 17:39:00 +0800

    Fixed bugs in xgb model fitter

--- open_competition/tabular/model_fitter.py
+++ open_competition/tabular/model_fitter.py
@@ -7,12 +7,14 @@ import multiprocessing
 from hyperopt import fmin, tpe, hp
 import hyperopt.pyll
 import xgboost as xgb
+import lightgbm as lgb
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import roc_auc_score
 import numpy as np
+
 cpu_count = multiprocessing.cpu_count()
 
 
@@ -29,15 +31,26 @@ class XGBOpt:
     colsample_bytree: hyperopt.pyll.base.Apply = hp.uniform('colsample_bytree', 0.3, 1)
     gamma: hyperopt.pyll.base.Apply = hp.choice('gamma', [0, 1, 5])
 
+
 @dataclass
 class LGBOpt:
-    num_threads:hyperopt.pyll.base.Apply = hp.choice('num_threads', [cpu_count])
+    num_threads: hyperopt.pyll.base.Apply = hp.choice('num_threads', [cpu_count])
+    num_leaves: hyperopt.pyll.base.Apply = hp.choice('num_leaves', [64])
+    metric: hyperopt.pyll.base.Apply = hp.choice('metric', ['binary_error'])
+    num_rounds: hyperopt.pyll.base.Apply = hp.choice('num_rounds', [6000])
+    objective: hyperopt.pyll.base.Apply = hp.choice('objective', ['binary'])
+    learning_rate: hyperopt.pyll.base.Apply = hp.uniform('learning_rate', 0.01, 0.1)
+    feature_fraction: hyperopt.pyll.base.Apply = hp.uniform('feature_fraction', 0.5, 1.0)
+    bagging_fraction: hyperopt.pyll.base.Apply = hp.uniform('bagging_fraction', 0.8, 1.0)
+
 
 class FitterBase(object):
-    def __init__(self, label, metric):
+    def __init__(self, label, metric, max_eval=100, opt=None):
         self.label = label
         self.metric = metric
         self.opt_params = dict()
+        self.max_eval = max_eval
+        self.opt = opt
 
     def get_loss(self, y, y_pred):
         if self.metric == 'error':
@@ -55,16 +68,21 @@ class FitterBase(object):
         else:
             raise Exception("Not implemented yet.")
 
+    def get_rand_param(self):
+        if self.opt is None:
+            return None
+        else:
+            return hyperopt.pyll.stochastic.sample(asdict(self.opt))
+
 
 class XgBoostFitter(FitterBase):
-    def __init__(self,  label='label', metric='error', opt: XGBOpt = None, max_eval=100):
-        super(XgBoostFitter, self).__init__(label, metric)
+    def __init__(self, label='label', metric='error', opt: XGBOpt = None, max_eval=100):
+        super(XgBoostFitter, self).__init__(label, metric, max_eval)
         if opt is not None:
             self.opt = opt
         else:
             self.opt = XGBOpt()
         self.clf = None
-        self.max_eval = max_eval
 
     def train(self, train_df, eval_df, params=None, use_early_stop=True, verbose_eval=False):
         dtrain = xgb.DMatrix(train_df.drop(columns=[self.label]), train_df[self.label])
@@ -81,12 +99,12 @@ class XgBoostFitter(FitterBase):
             self.clf = xgb.train(use_params, dtrain, num_boost_round=params['num_round'], evals=evallist,
                                  verbose_eval=verbose_eval)
 
-    def search(self, train_df, eval_df):
+    def search(self, train_df, eval_df, use_early_stop=True, verbose_eval=False):
         self.opt_params = dict()
         deval = xgb.DMatrix(eval_df.drop(columns=[self.label]))
 
         def train_impl(params):
-            self.train(train_df, eval_df, params)
+            self.train(train_df, eval_df, params, use_early_stop=use_early_stop, verbose_eval=verbose_eval)
             if self.metric == 'auc':
                 y_pred = self.clf.predict(deval)
             else:
@@ -95,7 +113,7 @@ class XgBoostFitter(FitterBase):
 
         self.opt_params = fmin(train_impl, asdict(self.opt), algo=tpe.suggest, max_evals=self.max_eval)
 
-    def search_k_fold(self, k_fold, data):
+    def search_k_fold(self, k_fold, data, use_early_stop=True, verbose_eval=False):
         self.opt_params = dict()
 
         def train_impl_nfold(params):
@@ -103,7 +121,7 @@ class XgBoostFitter(FitterBase):
             for train_id, eval_id in k_fold.spilt(data):
                 train_df = data.loc[train_id]
                 eval_df = data.loc[eval_id]
-                self.train(train_df, eval_df, params)
+                self.train(train_df, eval_df, params, use_early_stop=use_early_stop, verbose_eval=verbose_eval)
                 deval = xgb.DMatrix(eval_df.drop(columns=[self.label]))
                 if self.metric == 'auc':
                     y_pred = self.clf.predict(deval)
@@ -117,18 +135,21 @@ class XgBoostFitter(FitterBase):
     def get_rand_param(self):
         return hyperopt.pyll.stochastic.sample(asdict(self.opt))
 
-    def train_k_fold(self, k_fold, data, column_name='xgb_predict', params=None, use_early_stop=True,
-                     verbose_eval=False):
-        data_copy = data.copy(deep=True)
+    def train_k_fold(self, k_fold, train_data, test_data, params=None, use_early_stop=True,
+                     verbose_eval=False, drop_test_y=True):
         if params is not None:
             use_params = params
         else:
             use_params = self.opt_params
-        data_copy[column_name] = np.NaN
-
-        for train_id, eval_id in k_fold.split(data_copy):
-            train_df = data_copy.loc[train_id]
-            eval_df = data_copy.loc[eval_id]
+        train_pred = np.ndarray([np.NaN for x in range(train_data.shape[0])])
+        test_pred = np.ndarray([0 for x in range(test_data.shape[0])])
+        if drop_test_y:
+            dtest = xgb.DMatrix(test_data.drop(columns=self.label))
+        else:
+            dtest = xgb.DMatrix(test_data)
+        for train_id, eval_id in k_fold.split(train_data):
+            train_df = train_data.loc[train_id]
+            eval_df = train_data.loc[eval_id]
             dtrain = xgb.DMatrix(train_df.drop(columns=[self.label]), train_df[self.label])
             deval = xgb.DMatrix(eval_df.drop(columns=[self.label]), eval_df[self.label])
             evallist = [(deval, 'eval')]
@@ -138,13 +159,49 @@ class XgBoostFitter(FitterBase):
             else:
                 clf = xgb.train(use_params, dtrain, num_boost_round=params['num_round'], evals=evallist,
                                 verbose_eval=verbose_eval)
-            y_pred = clf.predict(deval)
-            data_copy.loc[eval_id, column_name] = y_pred
-        return data_copy[column_name]
+            train_pred[eval_id] = clf.predict(deval)
+            test_pred += clf.predict(dtest)
+        test_pred = test_pred / k_fold.n_splits
+        return train_pred, test_pred
 
 
 class LGBFitter(FitterBase):
-    def __init__(self, label='label', metric='error', opt: XGBOpt = None, max_eval=100):
+    def __init__(self, label='label', metric='error', opt: LGBOpt = None, max_eval=100):
+        super(LGBFitter, self).__init__(label, metric, max_eval)
+        if opt is not None:
+            self.opt = opt
+        else:
+            self.opt = LGBOpt()
+
+    def train(self, train_df, eval_df, params=None, use_early_stop=True, verbose_eval=False):
+        dtrain = lgb.Dataset(train_df.drop(columns=[self.label]), train_df[self.label])
+        deval = lgb.Dataset(eval_df.drop(columns=[self.label]), eval_df[self.label])
+        evallist = [deval]
+        if params is None:
+            use_params = self.opt_params
+        else:
+            use_params = params
+        if use_early_stop:
+            self.clf = lgb.train(use_params, dtrain, num_boost_round=params['num_round'], valid_sets=evallist,
+                                 early_stopping_rounds=params['early_stopping_rounds'], verbose_eval=verbose_eval)
+        else:
+            self.clf = lgb.train(use_params, dtrain, num_boost_round=params['num_round'], valid_sets=evallist,
+                                 verbose_eval=verbose_eval)
+
+    def search(self, train_df, eval_df):
+        self.opt_params = dict()
+        deval = lgb.Dataset(eval_df.drop(columns=[self.label]))
+
+        def train_impl(params):
+            self.train(train_df, eval_df, params)
+            if self.metric == 'auc':
+                y_pred = self.clf.predict(deval)
+            else:
+                y_pred = (self.clf.predict(deval) > 0.5).astype(int)
+            return self.get_loss(eval_df[self.label], y_pred)
+
+        self.opt_params = fmin(train_impl, asdict(self.opt), algo=tpe.suggest, max_evals=self.max_eval)
+
 
 # class ModelFitter:
 #     def __init__(self, default_dict, search_config):
