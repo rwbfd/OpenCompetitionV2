cd423e330e081908e510eec4c8730c33459bac11
rwbfd <ran.wang.math@gmail.com>
2020-11-04 22:39:52 +0800

    Added pytorch rl

---
+++ open_competition/optimization/rl/core/a2c.py
@@ -0,0 +1,22 @@
+import torch
+
+
+def a2c_step(policy_net, value_net, optimizer_policy, optimizer_value, states, actions, returns, advantages, l2_reg):
+
+    """update critic"""
+    values_pred = value_net(states)
+    value_loss = (values_pred - returns).pow(2).mean()
+    # weight decay
+    for param in value_net.parameters():
+        value_loss += param.pow(2).sum() * l2_reg
+    optimizer_value.zero_grad()
+    value_loss.backward()
+    optimizer_value.step()
+
+    """update policy"""
+    log_probs = policy_net.get_log_prob(states, actions)
+    policy_loss = -(log_probs * advantages).mean()
+    optimizer_policy.zero_grad()
+    policy_loss.backward()
+    torch.nn.utils.clip_grad_norm_(policy_net.parameters(), 40)
+    optimizer_policy.step()
