9e151e1bd3207bf62a093cbb556e320f91720d51
calibertytz <calibertytz@163.com>
2020-12-15 17:20:38 +0800

    update for setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import numpy
 
 compile_flags = ['-std=c++11']
 
-module = Extension('cython/ind_cols',
+module = Extension('ind_cols',
                    ['cython/ind_cols.pyx'],
                    language='c++',
                    include_dirs=[numpy.get_include()],
